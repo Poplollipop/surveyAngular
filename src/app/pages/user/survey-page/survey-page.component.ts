69d6178d2d66cfb15d477203382b4ee6173c0aa3
Joker <aaa111334@gmail.com>
2025-02-23 22:16:04 +0800

    add status login login have to fix

--- src/app/pages/user/survey-page/survey-page.component.ts
+++ src/app/pages/user/survey-page/survey-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { LoginService } from '../../../@service/login.service';
 
 @Component({
   selector: 'app-survey-page',
@@ -13,7 +14,21 @@ import { Router } from '@angular/router';
   styleUrl: './survey-page.component.scss'
 })
 export class SurveyPageComponent {
-  constructor(private router: Router) { }
+
+  userStatus: boolean = false;
+
+  constructor(
+    private router: Router,
+    private loginservice: LoginService,
+  ) { }
+
+  ngOnInit(): void {
+    // 訂閱登入狀態
+    this.loginservice.login$.subscribe(status => {
+      this.userStatus = status;
+      console.log(this.userStatus);
+    });
+  }
 
   toStatistics() {
     this.router.navigateByUrl('/statistics')
